3b2ce0ed9fab56363a92faeeed55160f7b538145
sunwei <swaysty@gmail.com>
2018-01-08 21:36:57 +0800

    add declaration of function init_spinor_field_zero

--- include/init.h
+++ include/init.h
@@ -14,9 +14,11 @@ extern void init_gauge_random(su3_field u, int iseed);
 
 extern void init_spinor_zero(spinor* s);
 
-extern void init_spinor_unit(spinor_field s);
+extern void init_spinor_field_zero(spinor_field s);
 
-extern void init_spinor_random(spinor_field s, int iseed);
+extern void init_spinor_field_unit(spinor_field s);
+
+extern void init_spinor_field_random(spinor_field s, int iseed);
 
 // allocate memory for su3_field
 extern void su3_field_alloc(su3_field** u);
